0e8046dc168bc8ccd556f78bd03e7927dc02414a
jmhar <jmhar@protonmail.com>
2021-08-17 14:53:35 +1000

    extract palette menu

---
+++ src/components/palette/UploadPalette.js
@@ -0,0 +1,33 @@
+import { Component } from 'react';
+import uploadIcon from '../../images/right-arrow.png';
+
+export default class PaletteTools extends Component {
+    state = {
+        upload: false
+    };
+
+    showUpload = () => {
+        this.setState({ upload: true })
+    }
+
+    hideUpload = () => {
+        this.setState({ upload: false })
+    }
+
+    uploadPalette = () => {
+
+    }
+
+    render() {
+        const { upload } = this.state
+        return (
+            <>
+                <input type="text" placeholder="Name Your Creation" id="name-swatch" />
+                <div class="palette-menu-item" onMouseEnter={this.showUpload} onMouseLeave={this.hideUpload} onClick={this.uploadPalette}>
+                    <img src={uploadIcon} alt="upload" style={{ display: (upload ? 'none' : 'block') }} />
+                    <h4 style={{ display: (upload ? 'block' : 'none') }}>Upload</h4>
+                </div>
+            </>
+        )
+    }
+}
