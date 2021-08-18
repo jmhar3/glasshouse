5ee99df4ef75ca7cdb3d6f441e6ea97b3eca0791
jmhar <jmhar@protonmail.com>
2021-08-18 18:10:18 +1000

    homepage background update events

--- src/components/swatches/SwatchCard.js
+++ src/components/swatches/SwatchCard.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const SwatchCard = ({ name, colours, index, setBackground }) => (
-    <div className="swatch-card" key={index} onMouseEnter={setBackground}>
+const SwatchCard = ({ name, colours, index, mouseEnter, mouseLeave }) => (
+    <div className="swatch-card" key={index} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
         <div className="card-head">
             <Link to="/palette" className="swatch-name">{name}</Link>
             <h4 className="like-button">❤️</h4>
--- src/components/swatches/Swatches.js
+++ src/components/swatches/Swatches.js
@@ -7,20 +7,24 @@ export default class Swatches extends Component {
         super();
         this.state = {
             name: "no name creation",
-            colours: [[214,78,69],[247,242,163],[201,216,147],[57,141,112],[62,80,64]],
+            colours: [[42, 41, 48], [90, 83, 84], [191, 157, 175], [188, 138, 125], [215, 170, 66]],
             background: randomColor()
         };
     }
 
-    renderPanels = () => {
+    mouseEnter = () => {
         const swatchColours = Array.from(this.state.colours)
         const colours =             swatchColours.map((colour, index) => `, rgba(${colour[0]},${colour[1]},${colour[2]},1)`)
         console.log(colours.join(''))
-        return colours.join('');
+        document.querySelector('body').style.background = `linear-gradient(to right${colours.join('')})`
+    }
+
+    mouseLeave = () => {
+        document.querySelector('body').style.background = this.state.background
     }
 
     componentDidMount() {
-        document.querySelector('body').style.background = `linear-gradient(to right${this.renderPanels()})`
+        this.mouseLeave()
         // fetch('http://colormind.io/api/', {
         // method: 'POST',
         // body: JSON.stringify({
@@ -34,10 +38,9 @@ export default class Swatches extends Component {
     }
 
     render() {
-        console.log(this.state.colours)
         return (
             <>
-                <SwatchCard name={this.state.name} colours={this.state.colours} />
+                <SwatchCard name={this.state.name} colours={this.state.colours} mouseLeave={this.mouseLeave} mouseEnter={this.mouseEnter} />
             </>
         )
     }
